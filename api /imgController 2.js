056dc70591d19a545d584794a3a1d5ba1c94e382
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2022-04-24 12:12:10 -0300

    Fatorando

---
+++ api /imgController 2.js
@@ -0,0 +1,77 @@
+class ImgController {
+
+  constructor() {
+    this.foto = [
+      {
+        id: 1,
+        url: "https://media3.giphy.com/media/3o6gEgqmj8VpFdMxY4/200.webp?cid=ecf05e47ainfryod3p3xctx351u1z00cuwzuz96bppe1ijmn&rid=200.webp&ct=g",
+        alt: "Michael Jodan",
+        description: "Gift do lendánrio Michael Jodan",
+      },
+      {
+        id: 2,
+        url: "https://media0.giphy.com/media/uxTYtxv3Ezryg/200.webp?cid=ecf05e47ainfryod3p3xctx351u1z00cuwzuz96bppe1ijmn&rid=200.webp&ct=g",
+        alt: "Michael Jodan",
+        description: "Gift do lendário Michael Jodan",
+      },
+      {
+        id: 3,
+        url: "https://media0.giphy.com/media/e5RONJDZLaIRR5itrT/200w.webp?cid=ecf05e47124k0udtb3v43x94xa1rdj84fug95ogd13zu49mj&rid=200w.webp&ct=g",
+        alt: "Lebron James",
+        description: "Gift do papai Lebron James",
+      },
+      {
+        id: 4,
+        url: "https://media4.giphy.com/media/3oEdv9kR4Jsl05gS4w/200w.webp?cid=ecf05e47124k0udtb3v43x94xa1rdj84fug95ogd13zu49mj&rid=200w.webp&ct=g",
+        alt: "Lebron James",
+        description: "Gift do papai Lebron James",
+      },
+      {
+        id: 5,
+        url: "https://media1.giphy.com/media/eqWdbfp9VGD4gwQrIK/giphy.gif?cid=ecf05e47vpbsaqt2ceh1a7c1e88vele41kda3g1owmfhpx2k&rid=giphy.gif&ct=g",
+        alt: "Giannis Antetokounmpo",
+        description: "Gift do Deus Grego",
+      },
+      {
+        id: 6,
+        url: "https://media2.giphy.com/media/3oAt2dA6LxMkRrGc0g/200.webp?cid=ecf05e47vpbsaqt2ceh1a7c1e88vele41kda3g1owmfhpx2k&rid=200.webp&ct=g",
+        alt: "James Harden",
+        description: "Gift do Barba James Haden",
+      },
+      {
+        id: 7,
+        url: "https://media2.giphy.com/media/l0MYrguDYf6uvscak/giphy.webp?cid=ecf05e47vpbsaqt2ceh1a7c1e88vele41kda3g1owmfhpx2k&rid=giphy.webp&ct=g",
+        alt: "Damian Lillard",
+        description: "Gift do snipe americano Lillard",
+      },
+      {
+        id: 8,
+        url: "https://media3.giphy.com/media/3o6gE08CvPHCg3eG2s/200.webp?cid=ecf05e47vpbsaqt2ceh1a7c1e88vele41kda3g1owmfhpx2k&rid=200.webp&ct=g",
+        alt: "Michael Jordan",
+        description: "Gift do Rei de todos",
+      },
+      {
+        id: 9,
+        url: "https://media2.giphy.com/media/3o6gDTA8qvNp4RomIM/200.webp?cid=ecf05e47cifiznfr7uo0yhuk8nlihiyavfykay5hhaj5xr0s&rid=200.webp&ct=g",
+        alt: "Michael Jodan",
+        description: "Gift Não tem como parar o homem",
+      },
+    ];
+  }
+
+  listarImg() {
+    return this.foto;
+  }
+
+  listarPorId(req, res) {
+    let fotoConf;    
+    this.foto.forEach((foto)=>{
+      if(Number(foto.id) == Number(req.params.id)){
+        return fotoConf = foto;
+      }    
+    })
+    return fotoConf;
+  }
+}
+
+module.exports = ImgController;
